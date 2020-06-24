af3b389e31ba82cdd4f8eff21925debb281f6c59
BoKiMoMo <aacd870624@gmail.com>
2020-06-25 00:14:06 +0800

    push6-07~6-10

--- chapterDemo/6-Chapter/6-7-FunctionTemplate/FunctionTemplate.h
+++ chapterDemo/6-Chapter/6-7-FunctionTemplate/FunctionTemplate.h
@@ -1,14 +1,13 @@
-template < typename T >  // or template< class T >
-T maximum( T value1, T value2, T value3 )
+template <typename T>
+T maximum(T value1, T value2, T value3)
 {
-   T maximumValue = value1; 
+	T maximumValue = value1;
 
-   if ( value2 > maximumValue )
-      maximumValue = value2;
+	if (value2 > maximumValue)
+		maximumValue = value2;
 
-   if ( value3 > maximumValue )
-      maximumValue = value3;
+	if (value3 > maximumValue)
+		maximumValue = value3;
 
-   return maximumValue;
+	return maximumValue;
 }
-
